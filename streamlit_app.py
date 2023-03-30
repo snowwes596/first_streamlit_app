3f54c817d480159ee55716d2bf780be974687d3f
snowwes596 <129352369+snowwes596@users.noreply.github.com>
2023-03-30 11:14:55 +0800

    Update streamlit_app.py
    Added Special Menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥣    Omerga 3 & Blueberry Oatmeal')
 streamlit.text('🥗    Kale, Spinach & Rocker Smoothie')
 streamlit.text('🐔    Hard-Boiled Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('Special Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
