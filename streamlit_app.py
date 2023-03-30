6978e839f23a3b341c058eb9e75885e29f8e4656
snowwes596 <129352369+snowwes596@users.noreply.github.com>
2023-03-30 11:13:57 +0800

    Update streamlit_app.py
    Changed Breakast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omerga 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocker Smoothie')
-streamlit.text('Hard-Boiled Eggs')
+streamlit.text('🥣    Omerga 3 & Blueberry Oatmeal')
+streamlit.text('🥗    Kale, Spinach & Rocker Smoothie')
+streamlit.text('🐔    Hard-Boiled Eggs')
+streamlit.text('🥑🍞 Avocado Toast')
